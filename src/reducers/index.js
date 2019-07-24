d37551ab82b7ca68e145122bbc8702bc831fb7fb
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-24 14:09:05 +0000

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT': return [...state,action.payload]
+    case 'REMOVE_INGREDIENT': return state.filter(item => item !== action.payload)
+    default: return state
+  }
 }
